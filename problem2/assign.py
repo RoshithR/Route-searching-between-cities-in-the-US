f484a2014d0089ce61173ee2472c371048204450
khushboo_sah <sahk@iu.edu>
2017-10-01 21:17:33 -0400

    my final version

--- problem2/assign.py
+++ problem2/assign.py
@@ -1,185 +1,158 @@
+#!/usr/bin/env python
+#(1) a description of how you formulated the search problem, including precisely dening the state space, the successor function,
+#the edge weights, the goal state, and (if applicable) the heuristic function(s) you designed, including an
+#argument for why they are admissible;
+#answer : Intially, I thought the ultimate goal of this problem is to reduce m and n factor. since, K is dependent on number of groups.
+#I assumed my intial state to be the member with the highest time.
+#goal state: to reduce time of each highest member to the lowest by groupig it with other memebers in group of two/ three. If grouping reduces its individual cost, else leave it alone.
+#sucessor function : individual with their costs, all possible combinations with rest of members in group of two or three and their time.
+#the edge weight: total time taken by considering factors like m, n and 1 min(meeting with instructor)  involved.
+
+
+#(2) a brief description of how your search algorithm works;
+#answer : it first converts the input file into matrix of 'n*4' size (wherein n is the number of users). Then, inserts all the users in list 'test'. Then,
+#1) it calculates the individual cost of all members(using function 'total_cost') and take the member with highest cost(using function 'returnmax'). in case of tie, it picks randomly.
+#2) 'sucessors' function for that highest member is being called. 'successor function' : gives all possible combination of highest member with rest of group, in group of three and sort them in
+# reverse order(just like priority queue). So, the group with lowest time will pop first.
+#to reduce k factor drastically, I forced my team to be in group of three.
+#3)Once any group gets finalized, then they are removed from 'test' list and same process been repeated for rest of the members, till the list of 'test' size reduces to 2 or 1.
+#4)In case 'test' size = 2 or 1(it will happen if number of member size is not divisible by 3), it groups them togther and calculates its time and prints them.
+
+
+#(3) and discussion of any problems you faced, any assumptions, simplications, and/or design decisions you made.
+#answer:Intially I tried coding my solution in such a manner that it will reduce m, n and 1 min(meeting with instructor). Wherein, I calculated the total time for individual members and picked 
+#the one with highest time.
+#And tried grouping it with other members, in group of two,picked group if time of 'group of two' less than 'individual'.and then tried grouping it rest of memebers, in group of three, picked group
+# if time of 'group of three' less than 'group of two'.
+#for example: from given input  total time taken for individual members were 'd'=7 , 'c'=0,'f'=0,'z'=0,'k'=7,'s'=0 ,(where n=3,m=5,k=7) (taken intials of each member's name from  given input file)
+#1)got two members with highest individual 'd' and 'k', so i picked one of them randomly, then tried grouping them into two. In case, I picked 'd' first,then
+# 'd,c'=4 , 'd,f'=10, 'd,z'=4 , 'd,k'=14, 'd,s'=7, picked that group which has lower cost than 'd'=7. so, I picked 'd,c' = 4. In case, I would have failed to find the lower time than  'd'
+# then I would have left 'd' as my final group.
+# Now, tried grouping 'd,c' with rest of the members and calculated their cost too.
+#'d,c,f'=9 , 'd,c,z'=2 , 'd,c,k'=7 , 'd,c,s'=4. picked that group which has lower cost than 'd,c'=4, so I got 'd,c,z'=2. In case, I failed to found the lower time than group 'd,c'
+#then I would have left 'd,c' as my final group.
+#Doing this, factors like m, n and 1 min(meeting with instructor) were reduced drastically but it was not reducing the k factor. 
+#so to avoid this issue, I changed my successor to produce only combinations of threes.
+
 import os
 import sys
 
-class Team:
-    team_members = []
-    cost = -1
 
-    def __repr__(self):
-        return str("{Team Members : " + str(self.team_members) + ", Cost : " + str(self.cost) + "}")
-
-def preffered_partner_cost (result) :
+def prefferedpartner(result) :
     cost_n = 0 
     for i in range (len(l)) :
-        
         sentence = l[i][2]
-        #print 'sent',sentence
         for j in range (len(result)) :
             if  result[j]== l[i][0] :
-    
                 if (sentence != '_') :
                     sentence = sentence.split (',')
-                    #print 'hey', sentence
                     s1 = set(sentence)
                     s2 = set(result)
                     if len(s1.intersection(s2)) != len(sentence) :
-
                         cost_n = cost_n + (len(sentence) - len(s1.intersection(s2)))
                     
                     else :
-                    
                         cost_n = cost_n + 0
-
     return cost_n
 
-def team_cost (result) :
+def teampreference(result) :
     cost_i = 0
     for i in range (len(l)) :
         for j in range (len(result)) :
             if(result[j] == l[i][0] ) :
-                
                 if ((len(result) != int(l[i][1])) and int(l[i][1]) != 0):
                     cost_i = cost_i + 1
                 else:
                     cost_i = cost_i + 0
     return cost_i
 
-def Nonpreffred_cost (result) :
+def Nonpreffredmember(result) :
     cost_m = 0
     for i in range (len(l)) :
         sentence = l[i][3]
         for j in range (len(result)) :
-
             if  result[j]== l[i][0] :
-            
                 if (sentence != '_') :
                     sentence = sentence.split (',')
                     s1 = set(sentence)
                     s2 = set(result)
-                    #print len(s1.intersection(s2))
                     if len(s1.intersection(s2))!= 0 :
-                        cost_m = cost_m +  len(s1.intersection(s2))
+                        cost_m = cost_m + len(s1.intersection(s2))
                     else :
                         cost_m = cost_m + 0
     return  cost_m
 
 def total_cost(result):   
-    p = team_cost(result)
+    x = teampreference(result)
+    y = prefferedpartner (result)
+    z = Nonpreffredmember(result)
+    total_sum = x * 1 + y * n + z * m
+    return total_sum
 
-    n = preffered_partner_cost (result)
-    m = Nonpreffred_cost(result)
-    
-    plus = p * 1 + n * 3 + m * 5
-    #print plus
-    return plus
-
-
-
-
-
+q=[]
+currentmax = []
 def successors(test):
-    max_cost = -1
+    max_cost = 0
     inter_teams = []
+    for a in range (len(test)):    
+        for b in range(a+1,len(test)):
+            for c in range(b+1,len(test)):
+                grpThree=  [test[a],test[b],test[c]]
+                cost = total_cost(grpThree)
+                q.append((cost,grpThree[:]))
+    q.sort(reverse = True)
+    return q        
 
-    #print 'hey', test
-    for a in range (len(test)):
-        
-        grpThree = [test[a]] 
-        #print 'hello',grpThree
-        cost = total_cost(grpThree)
-        #print 'two in grps:', cost,grpThree
-    
-        if cost >= max_cost: # and grpThree != []:
-            max_cost = cost
-            new_set = grpThree[:]
-            team = Team()
-            team.team_members = new_set
-            team.cost = max_cost
-            inter_teams.append(team) 
+input_file = sys.argv[1]
+k = int(sys.argv[2])
+m = int(sys.argv[3])
+n = int(sys.argv[4])
 
-            #print 'this is',inter_teams
-            for b in  range (len(test)):
-                if test[a]!=test[b]:
-                    grpThree=  [test[a],test[b]]
-                    cost = total_cost(grpThree)
-                    #print grpThree,cost
-                    if max_cost >= cost :
-                        min_cost1 = cost
-                        new_set1 = grpThree[:]
-                        team = Team()
-                        team.team_members = new_set1
-                        team.cost = min_cost1
-                        inter_teams.append(team)
-               
-                        #print inter_teams[0]
-                        
-                        #print min_cost1,new_set1
-                        for c  in  range (len(test)):
-                            if test[a]!=test[b] and test[b]!=test[c] and test[c]!=test[a] :
-                                grpThree=  [test[a],test[b],test[c]]
-                                cost = total_cost(grpThree)
-                                #print grpThree,cost
-                                if min_cost1 >= cost :
-                                    mincost2 =cost
-                                    newset2 = grpThree[:]
-                                    team = Team()
-                                    team.team_members = newset2
-                                    team.cost = mincost2
-                                    inter_teams.append(team) 
-       
-        #   return inter_teams
-        
-#
-    return inter_teams        
-#    return inter_teams
-r =[]
-final_cost = 0
-
-file = open ( 'ip.txt' , 'r')
+file = open ( input_file , 'r')
 l = []
 l = [ line.split() for line in file]
-#print l
+
 
 test= []
 for i in range(len(l)):
     test.append(l[i][0])
-#print test
-
-
-min_val =0
-
-grpThree = []
-intermediate = []
-inter_teams = []
-inter_teams1 = []
-#final_cost =0
-final_grp =[]
-
-
-while test != []:
-    a=[]
-    minimum =100000
-    final_team =[]
-#test = ['D']
+currentmax=[]
+def returnmax(test):
+    minimum =-1
+    for i in range (len(test)):
+        grpThree = [test[i]] 
+        cost = total_cost(grpThree)
+        currentmax.append((cost,grpThree[:]))
+        if(currentmax[i][0]>minimum):
+            minimum = currentmax[i][0]
+            team = grpThree[:]    
+    return minimum,team
+  
+        
+finl_grp=[]
+temp_grp=[]
+total_time =0
+while len(test)>2:
+    t=[]
+    t= returnmax(test)
     a= successors(test)
-    for s in a :
-        #print s
-        if minimum >= s.cost:
-            #print s.cost,s.team_members
-            minimum = s.cost
-            final_team= s.team_members
-    r.append(final_team)
-    test = list(set(test) - set(final_team))
-    final_cost= final_cost + minimum
-    #print 'hey',test 
-    print(" ".join(str(x) for x in final_team))
-    #print 'final teams are :' ,final_team,minimum
-
-
-
-#print r
-final_cost = final_cost + len(r) * 7
-print final_cost
+    min_time = 100000
+    for i in range (len(a)):
+        if set(t[1]).issubset(set(a[i][1])) and min_time> a[i][0]  and set(a[i][1]).issubset(set(test)):
+            min_time = a[i][0]
+            finl_grp = a[i][1]
+    temp_grp.append(finl_grp)
+    total_time = total_time + min_time
+    
+    
+    print(" ".join(str(x) for x in finl_grp))
+    test = list(set(test) - set(finl_grp))
+   
+if(len(test)!=0):
+    print(" ".join(str(x) for x in test))
+    temp_grp.append(test)
+total_time = total_time + total_cost(test) +len(temp_grp)*k #K
+print total_time
 
 
 
